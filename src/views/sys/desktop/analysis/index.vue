abd5f4e765d618a9864bc4d6f5b126b04fc56bd9
syj <162141495@qq.com>
2024-03-04 14:36:44 +0800

    first commit

---
+++ src/views/sys/desktop/analysis/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <GrowCard class="enter-y" />
+    <SiteAnalysis class="!my-4 enter-y" />
+    <div class="md:flex enter-y">
+      <VisitRadar class="md:w-1/3 w-full" />
+      <VisitSource class="md:w-1/3 !md:mx-4 !md:my-0 !my-4 w-full" />
+      <SalesProductPie class="md:w-1/3 w-full" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup name="Analysis">
+  import GrowCard from './components/GrowCard.vue';
+  import SiteAnalysis from './components/SiteAnalysis.vue';
+  import VisitSource from './components/VisitSource.vue';
+  import VisitRadar from './components/VisitRadar.vue';
+  import SalesProductPie from './components/SalesProductPie.vue';
+</script>
